6879c9a1f00fb1feacef24f9c100f47c34c153cb
Tosin Kupolati <kupolatioluwatosin@gmail.com>
2022-08-24 20:29:13 +0100

    Updated Docs

--- app.js
+++ app.js
@@ -39,14 +39,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// app.get('/', (req, res) => {
-//   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
-// });
-// app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
 app.get('/', (req, res) => {
-  res.send('<h1>Jobs API<h1>');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
